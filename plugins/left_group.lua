4faf03f26b72d7fef623e8b8bbf9cde8d1c23945
Mohammad Mahdi <mohammad400ebrahimi@yahoo.com>
2016-03-16 23:44:08 +0330

    Update left_group.lua

--- plugins/left_group.lua
+++ plugins/left_group.lua
@@ -1,9 +1,9 @@
 do
 -- Will leave the group if be added
 local function run(msg, matches)
-local bot_id = 182195776 -- your bot id
+local bot_id = 192522449 -- your bot id
    -- like local bot_id = 1234567
-    if matches[1] == 'leavenod32' and is_admin(msg) then
+    if matches[1] == 'leaveavira' and is_admin(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     elseif msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
       send_large_msg("chat#id"..msg.to.id, 'this is not one of my groups fuck you (coming soon u Globall Banned and Blocked of this bot KOSE NANAT️).', ok_cb, false)
@@ -20,7 +20,6 @@ return {
   run = run
 }
 end
---Copyright and edit; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
---کپی بدون ذکر منبع حرام است
+-- مدیر : @mohammadarak
+-- ربات : @avirabot
+-- هر گونه کپی برداری بدون ذکر منبع حرام است 
