458f6551f7fc063bd04553636f837e0ea3f9e71c
Mohammad Mahdi <mohammad400ebrahimi@yahoo.com>
2016-03-16 22:59:47 +0330

    Update all.lua

--- plugins/all.lua
+++ plugins/all.lua
@@ -154,7 +154,6 @@ return {
   run = run
 }
 end
---Copyright; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
---کپی بدون ذکر منبع حرام است
+-- مدیر : @mohammadarak
+-- ربات : @avirabot
+-- هر گونه کپی برداری بدون ذکر منبع حرام است 
